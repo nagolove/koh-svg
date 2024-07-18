8cdf4010fb561b0856a4e679bec31aaca87c073d
nagolove <dekar.own@gmail.com>
2024-07-19 01:05:56 +0300

    generated file

--- bld.lua
+++ bld.lua
@@ -1,35 +1,36 @@
 return {
 
+
    {
+
       not_dependencies = {
+
+
          "lfs",
+
+
          "resvg",
       },
+
       artifact = "mgc",
+
       main = "mgc_main.c",
+
       src = "src",
+
       exclude = {},
+
 
 
       debug_define = {
          ["BOX2C_SENSOR_SLEEP"] = "1",
       },
+
       release_define = {},
 
 
 
    },
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
